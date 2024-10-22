ba3100162827eff3bdd9f8cc3fb1816197b97b70
Abdulhamid <abdulhamidhersi@gmail.com>
2024-10-22 10:22:13 +0300

    comp

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const dogImageContainer = document.getElementById("dog-image-container");
+const dogBreeds = document.getElementById("dog-breeds");
+const dropdown = document.getElementById("breed-dropdown");
+
+
+function fetchAndAppendDogs() {
+  fetch(imgUrl)
+    .then((response) => response.json())
+    .then((dogs) => {
+      dogs.message.forEach((dog) => {
+        const image = document.createElement("img");
+        image.src = dog;
+        dogImageContainer.appendChild(image);
+      });
+    });
+}
+
+fetchAndAppendDogs();
+
+function fetchBreeds() {
+  fetch(breedUrl)
+    .then((res) => res.json())
+    .then((breeds) => {
+      const data = Object.keys(breeds.message);
+      appendBreed(data);
+    });
+}
+
+function appendBreed(data) {
+  dogBreeds.innerHTML = "";
+  data.forEach((breed) => {
+    const li = document.createElement("li");
+    li.textContent = breed;
+    dogBreeds.appendChild(li);
+li.addEventListener("click", (e) => {
+      e.target.style.color = "blue";
+    });
+  });
+}
+
+fetchBreeds();
+
+dropdown.addEventListener("change", (e) => {
+  fetch(breedUrl)
+    .then((res) => res.json())
+    .then((breeds) => {
+      const data = Object.keys(breeds.message);
+      const filteredData = data.filter((breed) =>
+        breed.startsWith(e.target.value)
+      );
+      appendBreed(filteredData);
+    });
+});
